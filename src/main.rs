b55f0fd13d5f5042fb3de6bf87714e2ffda503e8
etiescu <tiernanscully@gmail.com>
2020-11-03 12:46:16 +0100

    Actix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
+
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", name, id)
